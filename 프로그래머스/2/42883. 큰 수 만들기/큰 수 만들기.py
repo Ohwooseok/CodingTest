45fb0979a4dcbef1490b3d494cae9c126ab89ceb
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2025-09-24 16:58:25 +0900

    [level 2] Title: 큰 수 만들기, Time: 91.27 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def solution(number, k):
+    stack = []
+    for n in number:
+        
+        while k > 0 and stack and stack[-1] < n:
+            stack.pop()
+            k -= 1
+        
+        stack.append(n)
+    
+    if k > 0:
+        stack = stack[:-k]
+    
+    
+    return ''.join(stack)
