57b9945a12b3fab84af29eded95bcfed5c070439
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2026-03-03 21:09:16 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+out = []
+
+for _ in range(n):
+    cmd = input().split()
+
+    if cmd[0] == "push":
+        stack.append(int(cmd[1]))
+    elif cmd[0] == "pop":
+        out.append(str(stack.pop() if stack else -1))
+    elif cmd[0] == "size":
+        out.append(str(len(stack)))
+    elif cmd[0] == "empty":
+        out.append("0" if stack else "1")
+    else:  # top
+        out.append(str(stack[-1] if stack else -1))
+
+sys.stdout.write("\n".join(out))
