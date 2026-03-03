089dcc9dcb7071b391f447e05c2f4c24023be6c6
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2026-03-03 18:47:27 +0900

    [Silver V] Title: 너의 평점은, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,23 @@
+import sys
+import io
+from collections import defaultdict
+
+
+hak = {"A+":4.5, "A0" : 4.0, "B+" : 3.5, "B0" : 3.0, "C+" : 2.5, "C0" : 2.0, "D+" : 1.5, "D0" : 1.0 , "F" : 0.0}
+
+allhak = 0.0
+all = 0.0
+
+for _ in range(20):
+    name, num, hakk = input().split(" ")
+
+    if hakk == "P":
+        continue
+
+    num = float(num)
+
+    allhak += num
+
+    all += num * hak[hakk]
+
+print(all / allhak)
