34365fc051971ff0b5d4e3386f37eeb3cd9da9eb
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2026-03-11 21:14:28 +0900

    [Gold V] Title: 치킨 배달, Time: 268 ms, Memory: 37096 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,41 +1,51 @@
+import sys
+import io
+import math
+from collections import defaultdict
+from collections import deque
+from collections import Counter
 from itertools import combinations
-n, m = map(int,input().split())
-where = []
+import copy
 
+
+
+n, m = map(int, input().split())
+
+what = []
 for _ in range(n):
-    where.append(list(map(int, input().split())))
+    what.append(list(map(int, input().split())))
+
 house = []
 chicken = []
 
-
-# 치킨집과 집의 좌표 정보 리스트에 저장
 for i in range(n):
     for j in range(n):
-        if where[i][j] == 1:
+        if what[i][j] == 1:
             house.append((i,j))
-        elif where[i][j] == 2:
+        elif what[i][j] == 2:
             chicken.append((i,j))
-
 
 
 def distance(i,j,k,l):
     return abs(i-k) + abs(j-l)
 
-def chicken_distance(h, c): # 정해진 집과 치킨집에 따라서 각각의 치킨 거리를 구하는 함수
+def chicken_distance(h,c):
 
     result = 0
+
     for i,j in h:
         mini = 1_000_000_000
         for k,l in c:
             mini = min(mini, distance(i,j,k,l))
 
+
         result += mini
 
     return result
 
-
 mini = 1_000_000_000
-for p in combinations(chicken, m):
-    mini = min(mini, chicken_distance(house, p))
+for hc in combinations(chicken, m):
+
+    mini = min(mini, chicken_distance(house,hc))
 
 print(mini)
