a33bd061f4962d3b4ee37e21435358cba37afe45
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2026-03-13 17:58:05 +0900

    [Silver I] Title: 트럭, Time: 108 ms, Memory: 37184 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,31 +1,39 @@
 import sys
 import io
+import math
+from collections import defaultdict
 from collections import deque
+from collections import Counter
+from itertools import combinations
+import copy
 
-n, w, l = map(int, input().split())
-# 트럭 수, 다리의 길이, 다리의 최대 하중
+n, w, L = map(int,input().split(" "))
+# n = 트럭의 수, w = 다리의 길이, L : 다리의 최대 하중
 
-truck = list(map(int, input().split()))
+truck = list(map(int, input().split(" ")))
 
+bridge = deque([0] * w)
 
-dari = deque([0] * w)
 time = 0
 cur_weight = 0
 
-while truck or cur_weight > 0 :
+while truck or cur_weight > 0:
 
     time += 1
-    now = dari.popleft() # 다리를 빠져나간 트럭
+    now = bridge.popleft()
     cur_weight -= now
 
-    if truck and sum(dari) + truck[0] <= l:
-        move = truck.pop(0)
-        dari.append(move)
-        cur_weight += move
+    if truck and truck[0] + cur_weight <= L:
+        cur_t = truck.pop(0)
+        bridge.append(cur_t)
+        cur_weight += cur_t
     else:
-        dari.append(0)
+        bridge.append(0)
 
-    if sum(dari) == 0:
+
+    if sum(bridge) == 0:
         break
+
 print(time)
+
 
